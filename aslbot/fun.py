72cebfd530067bb76f4d8c5748253bb7f73b2ce4
Iranox <tgwieprich@gmail.com>
2020-07-27 20:04:57 +0200

    fix function name

--- aslbot/fun.py
+++ aslbot/fun.py
@@ -15,15 +15,15 @@ class FunCog(commands.Cog):
         await ctx.send("https://tenor.com/view/party-hard-harrypotter-alan-rickman-dumbledore-gif-4934551")
 
     @commands.command(name='blau', help="Blau wie das Meer")
-    async def sun_command(self, ctx):
+    async def blau(self, ctx):
         await ctx.send("https://www.youtube.com/watch?v=TgDXGQYib8k")
 
     @commands.command(name='trap', help="Admiral Ackbar sagt:")
-    async def sun_command(self, ctx):
+    async def trap(self, ctx):
         await ctx.send("https://giphy.com/gifs/Z1LYiyIPhnG9O")
 
     @commands.command(name='darkside', help="Dark site of the force")
-    async def sun_command(self, ctx):
+    async def darkside(self, ctx):
         await ctx.send("https://gph.is/VxbsSv")
 
 def setup(bot):
