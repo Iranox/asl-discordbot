5868fb50bb4fda6a93a4b8ba8c4441629c836289
Iranox <tgwieprich@gmail.com>
2020-07-27 20:02:59 +0200

    more fun

--- aslbot/fun.py
+++ aslbot/fun.py
@@ -14,7 +14,17 @@ class FunCog(commands.Cog):
     async def sun_command(self, ctx):
         await ctx.send("https://tenor.com/view/party-hard-harrypotter-alan-rickman-dumbledore-gif-4934551")
 
+    @commands.command(name='blau', help="Blau wie das Meer")
+    async def sun_command(self, ctx):
+        await ctx.send("https://www.youtube.com/watch?v=TgDXGQYib8k")
 
+    @commands.command(name='trap', help="Admiral Ackbar sagt:")
+    async def sun_command(self, ctx):
+        await ctx.send("https://giphy.com/gifs/Z1LYiyIPhnG9O")
+
+    @commands.command(name='darkside', help="Dark site of the force")
+    async def sun_command(self, ctx):
+        await ctx.send("https://gph.is/VxbsSv")
 
 def setup(bot):
     bot.add_cog(FunCog(bot))
